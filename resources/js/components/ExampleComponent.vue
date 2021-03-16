f3caa442e14f2ba7b6e0cce1c9f40d0509463488
Pashtet456 <70090279+Pashtet456@users.noreply.github.com>
2021-03-16 10:19:50 +0500

    first commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+                    <img :src="require(`../../assets/img/${imageName}`).default" alt="Logo">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                image: 'resources/assets/img/vrazrabotke.png',
+                imageName: 'vrazrabotke.png',
+            };
+        }
+    }
+</script>
---
+++ resources/js/components/SecondComponent.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: 'SecondComponent',
+    };
+</script>
+
+<style scoped>
+
+</style>
